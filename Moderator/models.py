a983a96a2b92c2e9a025399db847de97a872f07e
Ashikur Rahman <42954116+AshikurRahman-sec@users.noreply.github.com>
2020-09-04 07:58:08 +0600

    updates model.py from ashik

--- Moderator/models.py
+++ Moderator/models.py
@@ -13,6 +13,9 @@ class User(AbstractUser):
     registration = models.BigIntegerField(null=True,blank=True)
     session = models.CharField(max_length=100,null= True,blank = True)
 
+    def __str__(self):
+        return self.username
+
 
 class Course(models.Model):
     title = models.CharField(max_length=255)
