654101b502f26388657ea05ae4cc29ffdb050ac0
Ashikur Rahman <42954116+AshikurRahman-sec@users.noreply.github.com>
2020-09-11 15:32:49 +0600

    updates url.py from ashik

--- Teacher/urls.py
+++ Teacher/urls.py
@@ -4,8 +4,14 @@ from .views import *
 app_name = 'teacher'
 
 urlpatterns = [
-    path('create/',Q_Create.as_view(),name='create'),
+    path('',Teacher_Home.as_view(),name='teacherhome'),
+    path('create/<category>',University_Problem_Create.as_view(),name='create'),
+    path('create/',Acm_Problem_Create.as_view(),name='acmcreate'),
     path('e_create/',Exam_create.as_view(),name="examcreate"),
     path('result/',Result_Show.as_view(),name= "result"),
     path('report/<int:id>',Report_Show.as_view(),name="report"),
+    path('compare/',Answer_Compare.as_view(),name="comare"),
+    path('take_attendence/<int:id>',Take_Attendence.as_view(),name="takeattendence"),
+    path('create_attendence/',Attendence.as_view(),name="createattendence"),
+    path('attendence_list/',Attendance_List.as_view(),name='attendecelist'),
 ] 
