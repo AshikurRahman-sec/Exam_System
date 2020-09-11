deb14845def8e967942bf206467bc1a960eac751
Ashikur Rahman <42954116+AshikurRahman-sec@users.noreply.github.com>
2020-09-11 15:31:24 +0600

    updates url.py from ashik

--- Student/urls.py
+++ Student/urls.py
@@ -4,11 +4,15 @@ from .views import *
 app_name = 'student'
 
 urlpatterns = [
+    path('',Student_Home.as_view(),name='studenthome'),
+    path('registration/',Exam_Registration.as_view(),name='examregistration'),
     path('p_list/',Problem_list.as_view(),name='problemlist'),
-    path('p_detail/<int:id>/',Problem_show.as_view(),name='problemdetail'),
+    path('exam_question_show/',Exam_Problem_Show.as_view(),name='examquestionshow'),
+    path('p_detail/<int:id>/',Acm_Problem_show.as_view(),name='problemdetail'),
     path('c_list/',Course_list.as_view(),name='courselist'),
     path('e_list/',Exam_list.as_view(),name='examlist'),
-    path('submit/',Submit.as_view(),name="submit"),
-    path('e_detail/<int:id>',Exam_Detail.as_view(),name="examdetail"),
+    path('submit/<q_set_title>/<int:no>/',Exam_Question_Submit.as_view(),name="examsubmit"),
+    path('submit/',Acm_Problem_Submit.as_view(),name="acmsubmit"),
+    path('e_detail/<int:id>/',Exam_Detail.as_view(),name="examdetail"),
     path('marks/',Mark.as_view(),name="marks"),
 ] 
