f06ab66f5cb48fc6a9372caba1dd4be0c6c4af07
Ashikur Rahman <42954116+AshikurRahman-sec@users.noreply.github.com>
2020-09-13 07:24:07 +0600

    Update models.py

--- Moderator/models.py
+++ Moderator/models.py
@@ -1,32 +1,28 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
 
 # Create your models here.
 
-class User(AbstractUser):
-    is_student = models.BooleanField(default=False)
-    is_teacher = models.BooleanField(default=False)
-    department = models.CharField(max_length=100,null=True,blank=True)
-    image = models.ImageField(upload_to='image/', height_field=None, width_field=None, max_length=100,null=True,blank=True)
-    create = models.DateTimeField(auto_now_add=True)
-    update = models.DateTimeField(auto_now=True)
-    registration = models.BigIntegerField(null=True,blank=True)
-    session = models.CharField(max_length=100,null= True,blank = True)
-
-    def __str__(self):
-        return self.username
-
-
 class Course(models.Model):
     title = models.CharField(max_length=255)
-    teacher = models.ForeignKey(User,on_delete=models.DO_NOTHING)
+    teacher = models.ForeignKey(User,on_delete=models.CASCADE)
     def __str__(self):
         return self.title
 
 
 class Section(models.Model):
     name = models.CharField(max_length=255)
-    course = models.OneToOneField(Course, on_delete=models.DO_NOTHING)
+    course = models.OneToOneField(Course, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
+
+class Department(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    code = models.PositiveIntegerField(blank=True, null=True)
+    head = models.ForeignKey(
+        User, on_delete=models.CASCADE, blank=True, null=True)
+    establish_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.name)
